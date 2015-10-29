d60971ef34b8a54b12488d7b98e45ca6daa1312b
LuchoManto <lucianomanto@gmail.com>
2015-10-29 15:56:21 -0300

    Revert "esto no anda"
    This reverts commit 28c3888e78d3a559e435b3ed7b6fe14116012997.

--- src/control_motor.c
+++ src/control_motor.c
@@ -14,26 +14,18 @@
 
 void config_PWM(void)
 {
-	LPC_GPIO2->FIODIR |= (1 << 0);
-
-	LPC_SC->PCLKSEL0 &= ~(1 << 13);
-	LPC_SC->PCLKSEL0 &= ~(1 << 12); // clock del PWM = SYSCLK / 4 = 25 MHz
+	LPC_SC->PCLKSEL1 &= ~(1 << 13);
+	LPC_SC->PCLKSEL1 &= ~(1 << 12); // clock del PWM = SYSCLK / 4 = 25 MHz
 
 	LPC_PINCON->PINSEL4 |= (1 << 0); // pinsel configurado para que P2.0 sea la salida de la señal del PWM
 
-	LPC_PWM1->MR0 = 65789; // Valor del periodo del PWM para que el periodo del PWM sea 380 Hz
-	LPC_PWM1->MR1 = 10000; // Valor del ciclo de trabajo del PWM. inicial en 0
-	LPC_PWM1->LER |= (1 << 1); // actualizo latch de match1
-	LPC_PWM1->LER |= (1 << 0); // actualizo latch de match0
-
-	LPC_PWM1->MCR |= (1 << 4); // reset en MR1
+	LPC_PWM1->MR1 = 65789; // Valor del periodo del PWM para que el periodo del PWM sea 380 Hz
+	LPC_PWM1->MR0 = 0; // Valor del ciclo de trabajo del PWM. inicial en 0
 
 	LPC_PWM1->PCR |= (1 << 9); // PWM1 output enable
 
 	LPC_PWM1->TCR |= (1 << 0); // habilitar el timer
 	LPC_PWM1->TCR |= (1 << 3); // poner el timer en modo PWM
-
-
 }
 
 void arrancar_motor(void)
@@ -49,8 +41,7 @@ void arrancar_motor(void)
 
 void set_Pwm(unsigned int num)
 {
-	LPC_PWM1->MR1 = num; // Valor del ciclo de trabajo del PWM.
-	LPC_PWM1->LER |= (1 << 1); // actualizo latch de match1
+	LPC_PWM1->MR0 = num; // Valor del ciclo de trabajo del PWM.
 }
 
 void delay(unsigned int num)
